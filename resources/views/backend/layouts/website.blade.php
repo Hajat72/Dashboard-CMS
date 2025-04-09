bb112551c83e12cd73fbf0c1dc8bfb32572c9d2a
unknown <hajathussain59@gmail.com>
2025-04-09 06:36:39 -0700

    dashboard

---
+++ resources/views/backend/layouts/website.blade.php
@@ -0,0 +1,6 @@
+@extends('backend.layouts.app')
+@section('backend')
+    @include('backend.component.header')
+    @include('backend.component.sidebar')
+    @include('backend.component.footer')
+@endsection
