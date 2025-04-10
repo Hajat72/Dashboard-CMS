658a66c9a4c6727a56b1839814e80d8c1a5df1ea
unknown <hajathussain59@gmail.com>
2025-04-10 03:29:00 -0700

    backend

--- resources/views/backend/layouts/website.blade.php
+++
@@ -1,6 +0,0 @@
-@extends('backend.layouts.app')
-@section('backend')
-    @include('backend.component.header')
-    @include('backend.component.sidebar')
-    @include('backend.component.footer')
-@endsection
