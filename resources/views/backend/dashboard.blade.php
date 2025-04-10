3a4b73dcc6a4191b84f70cc2d8f7792678652640
unknown <hajathussain59@gmail.com>
2025-04-10 04:00:54 -0700

    backend changes

--- resources/views/backend/dashboard.blade.php
+++ resources/views/backend/dashboard.blade.php
@@ -1,5 +1,5 @@
 @extends('backend.layouts.app')
-@section('backend')
+@section('main')
 <main id="main" class="main">
 
     <div class="pagetitle">
--- resources/views/backend/layouts/app.blade.php
+++ resources/views/backend/layouts/app.blade.php
@@ -41,7 +41,7 @@
 <body>
 @include('backend.component.header')
 @include('backend.component.sidebar')
-    @yield('backend')
+    @yield('main')
 @include('backend.component.footer')
  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
